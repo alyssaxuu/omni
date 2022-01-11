6dbe57d978780df063b578dde15cf7b279bfed9d
Alyssa X <hi@alyssax.com>
2022-01-11 16:13:08 +0000

    Add Omni on install without reloading

--- src/background.js
+++ src/background.js
@@ -1,5 +1,33 @@
 var actions = [];
 
+// Inject Omni on install
+chrome.manifest = chrome.runtime.getManifest();
+var injectIntoTab = function (tab) {
+    var scripts = chrome.manifest.content_scripts[0].js;
+    var i = 0, s = scripts.length;
+    for( ; i < s; i++ ) {
+        chrome.scripting.executeScript({
+						target: {tabId: tab.id},
+            files: [scripts[i]]
+        });
+    }
+}
+
+// Get all windows
+chrome.windows.getAll({
+    populate: true
+}, function (windows) {
+    var i = 0, w = windows.length, currentWindow;
+    for( ; i < w; i++ ) {
+        currentWindow = windows[i];
+        var j = 0, t = currentWindow.tabs.length, currentTab;
+        for( ; j < t; j++ ) {
+            currentTab = currentWindow.tabs[j];
+            injectIntoTab(currentTab);
+        }
+    }
+});
+
 // Clear actions and append default ones
 function clearActions() {
 	getCurrentTab().then((response) => {
