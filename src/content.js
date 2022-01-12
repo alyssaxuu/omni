626015302ed48537fb7c5999adb275d3bd4a2fb4
Alyssa X <hi@alyssax.com>
2022-01-12 15:15:14 +0100

    Removed logging

--- src/content.js
+++ src/content.js
@@ -148,7 +148,6 @@ $(document).ready(function(){
 	// Handle actions from the omni
 	function handleAction() {
 		var action = actions.find(x => x.title == $(".omni-item-active .omni-item-name").text());
-		console.log(action);
 		closeOmni();
 		if ($(".omni-extension input").val().toLowerCase().startsWith("/remove")) {
 			chrome.runtime.sendMessage({request:"remove", type:action.type, action:action});
